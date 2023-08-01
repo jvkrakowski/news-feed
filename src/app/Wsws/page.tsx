26d942f38d5faf11c0713de46c54fceb32c29259
J.V Krakowski <krakowski.jv@gmail.com>
2023-08-01 14:25:41 -0400

    edited back buttons

--- src/app/Wsws/page.tsx
+++ src/app/Wsws/page.tsx
@@ -11,12 +11,12 @@ export default function InterSoc() {
        </header> 
 
        <div className="grid grid-cols-6 grid-rows-1 gap-4">
-        <div className="box col-span-3">
-            <Link href="/">Back</Link>
-        </div>
-    <div className="box col-span-3 col-start-4">
-        <Link href="/InSoc">Next</Link>
-    </div>
+        <Link href="/"><div className="box col-span-3">
+            Back
+        </div></Link>
+    <Link href="/InSoc"><div className="box col-span-3 col-start-4">
+        Next
+    </div></Link>
     </div>
 
        <div className="grid grid-cols-6 grid-rows-1 gap-4">
